daa69917923bdc9b3009661c5f6167ca57d34ee5
Terje Norderhaug <terje@in-progress.com>
2022-10-20 15:13:45 -0700

    Increment version

--- src/main/clariform/core.cljs
+++ src/main/clariform/core.cljs
@@ -57,7 +57,7 @@
         (println "Options:")
         (println summary))
       (some? (:version options))
-      "0.0.2"
+      "0.0.3"
       (some? (:check options))
       (when (not-empty arguments)
         (doseq [item arguments]
