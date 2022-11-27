c42f4bb62650d9f6295cd6d33e9c905c6dbdc875
Terje Norderhaug <terje@in-progress.com>
2022-11-27 15:33:32 -0800

    Increment version

--- src/main/clariform/core.cljs
+++ src/main/clariform/core.cljs
@@ -17,7 +17,7 @@
     :refer [format-code]]
    [clariform.io :as io]))
 
-(def version-string "0.1.0")
+(def version-string "0.1.1")
 
 (defn printerr [& vals]
   (binding [*print-fn* *print-err-fn*]
