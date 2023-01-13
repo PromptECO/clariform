cf1749dfa707ce65efcfcf4a3c6c694b099eb974
Terje Norderhaug <terje@in-progress.com>
2023-01-13 14:14:43 -0800

    Bump version

--- src/main/clariform/core.cljs
+++ src/main/clariform/core.cljs
@@ -19,7 +19,7 @@
    [clariform.transform :as transform
     :refer [transform]]))
 
-(def version-string "0.1.6")
+(def version-string "0.2.0")
 
 (defn printerr [& vals]
   (binding [*print-fn* *print-err-fn*]
